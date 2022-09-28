5e822c95ea4ff118588abdcdb7f5490b54b56a96
Riyaaz <rahmatullaha845@gmail.com>
2022-09-28 20:54:26 +0530

    batch-0.0.0

--- system_info.bat
+++ system_info.bat
@@ -23,5 +23,4 @@ ECHO NETWORK INFO
 ECHO ============================
 ipconfig | findstr IPv4
 ipconfig | findstr IPv6
-START https://support.microsoft.com/en-us/windows/windows-10-system-requirements-6d4e9a79-66bf-7950-467c-795cf0386715
 PAUSE
